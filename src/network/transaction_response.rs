cf76450777ef2e301cf11310df12423aebb8a196
Daniyar Itegulov <ditegulov@gmail.com>
2024-12-05 18:09:55 +1100

    derive `PartialEq` for response/receipt

--- src/network/transaction_response.rs
+++ src/network/transaction_response.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(
     into = "serde_from::TransactionEither",
     from = "serde_from::TransactionEither"
